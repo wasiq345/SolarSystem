9f0aed7989f80d9237b7e2f426ab64acb55df21c
wasiq345 <wasiqazeem000@gmail.com>
2025-07-20 23:25:58 +0500

    deleted build file

--- main.cpp
+++ main.cpp
@@ -104,7 +104,7 @@ int main()
 	camera.target = { GetScreenWidth()/2.0f, GetScreenHeight()/2.0f }; 
 	camera.offset = { GetScreenWidth()/2.0f, GetScreenHeight()/2.0f }; 
 	camera.rotation = 0.0f;
-	camera.zoom = 1.0f; 
+	camera.zoom = 0.5f;
 
 	Sun sun;
 	Texture2D earth = LoadTexture("graphics/earth.png");
